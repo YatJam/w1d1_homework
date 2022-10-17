d7eb15553b5490511ee8f7ba8825004ac005bf38
james yates <yates_7@hotmail.com>
2022-10-17 18:19:50 +0100

    first draft

---
+++ precourse_recap.py
@@ -0,0 +1,11 @@
+carrots = 11
+people = 3.5
+demeanor = "aggressive"
+
+if carrots >= people:
+    print("There are", carrots * people, demeanor, "carrots")
+else:
+    print("we are safe for now")
+print("Only the sea can protect us")
+
+
